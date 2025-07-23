f82f792e7e9ade75975a8d494cd95e9d23dc780a
zalatasama <zalatasama@gmail.com>
2025-07-23 08:26:42 +0000

    docs: công thức tính diện tích cần sơn

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,16 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
+import {
+  isRouteErrorResponse,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'react-router';
 import './app.css';
 import { ThemeProvider } from '@/shared/context/theme-provider';
 import i18n from '@/infrastructure/i18n/i18n';
 import { I18nextProvider } from 'react-i18next';
+import type { Route } from './+types/root';
 
 export const links = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -42,5 +50,34 @@ export default function App() {
         <Outlet />
       </ThemeProvider>
     </I18nextProvider>
+  );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = 'Oops!';
+  let details = 'An unexpected error occurred.';
+  let stack: string | undefined;
+
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? '404' : 'Error';
+    details =
+      error.status === 404
+        ? 'The requested page could not be found.'
+        : error.statusText || details;
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message;
+    stack = error.stack;
+  }
+
+  return (
+    <main className="pt-16 p-4 container mx-auto">
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre className="w-full p-4 overflow-x-auto">
+          <code>{stack}</code>
+        </pre>
+      )}
+    </main>
   );
 }
--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,5 @@ import { type RouteConfig, route } from '@react-router/dev/routes';
 export default [
   route('/', 'features/dashboard/layout.tsx', [
     route('', 'features/dashboard/index.tsx'),
-    route('paint-calculator', 'features/paint-calculator/index.tsx'),
   ]),
 ] satisfies RouteConfig;
